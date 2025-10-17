fe00b0afa327739b0c3cc05fa5bf20923415488d
Perez Milagros Camila <camilap11517@gmail.com>
2025-10-17 10:33:31 -0300

    Modificaciones en Program.cs para conectar con la base Tienda.db

--- Program.cs
+++ Program.cs
@@ -1,36 +1,36 @@
 ﻿using Microsoft.Data.Sqlite;
-string connectionString = "Data Source=base_test.db;";
+string connectionString = "Data Source=Tienda.db;";
 
 // Crear conexión a la base de datos
 using (SqliteConnection connection = new SqliteConnection(connectionString))
 {
     connection.Open();
     // Crear tabla si no existe
-    // por lo general este tipo de consultas no se implementa en un porgrama real
+    // por lo general este tipo de consultas no se implementa en un programa real
     // la aplicamos para poder crear nuestra base de datos desde cero
-    string createTableQuery = "CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY, nombre TEXT, precio REAL)";
+    string createTableQuery = "CREATE TABLE IF NOT EXISTS Producto (id_producto INTEGER PRIMARY KEY, descripcion TEXT, precio NUMERIC)";
     using (SqliteCommand createTableCmd = new SqliteCommand(createTableQuery, connection))
     {
         createTableCmd.ExecuteNonQuery();
-        Console.WriteLine("Tabla 'productos' creada o ya existe.");
+        Console.WriteLine("Tabla 'Producto' creada o ya existe.");
     }
     
     // Insertar datos
-    string insertQuery = "INSERT INTO productos (nombre, precio) VALUES ('Manzana', 0.50), ('Banana', 0.30)";
+    string insertQuery = "INSERT INTO Producto (descripcion, precio) VALUES ('Auriculares Philips', 45000), ('Auriculares JBL', 90000)";
             using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
             {
                 insertCmd.ExecuteNonQuery();
-                Console.WriteLine("Datos insertados en la tabla 'productos'.");
+                Console.WriteLine("Datos insertados en la tabla 'Producto'.");
             }
     // Leer datos
-            string selectQuery = "SELECT * FROM productos";
+            string selectQuery = "SELECT * FROM Producto";
             using (SqliteCommand selectCmd = new SqliteCommand(selectQuery, connection))
             using (SqliteDataReader reader = selectCmd.ExecuteReader())
             {
-                Console.WriteLine("Datos en la tabla 'productos':");
+                Console.WriteLine("Datos en la tabla 'Producto':");
                 while (reader.Read())
                 {
-                    Console.WriteLine($"ID: {reader["id"]}, Nombre: {reader["nombre"]}, Precio: {reader["precio"]}");
+                    Console.WriteLine($"ID: {reader["id_producto"]}, Descripcion: {reader["descripcion"]}, Precio: {reader["precio"]}");
                 }
             }
 
